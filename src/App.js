24d9f956c9c8cec4cc3b463fec978f3c85d465b9
Mankler <matt_rey@me.com>
2021-01-08 15:28:55 -0800

    added delete button

--- src/App.js
+++ src/App.js
@@ -2,32 +2,46 @@ import React, { Component } from 'react'
 import Table from './Table'
 
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters:[
       {
-        name: 'Charlie',
-        job: 'Janitor',
+        name:'Charlie',
+        job:'Janitor'
       },
       {
         name: 'Mac',
-        job: 'Bouncer',
+        job:'Bouncer'
       },
       {
-        name: 'Dee',
-        job: 'Aspring actress',
+        name:'Dee',
+        job:'Aspiring actress'
       },
       {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
+        name:'Dennis',
+        job:'Bartender'
+      }
     ]
+  }
+  render() {
+    const {characters} = this.state
 
     return (
   <div className="container">
-    <Table characterData={characters} />
+    <Table characterData={characters} removeCharacter={this.removeCharacter} />
   </div>
 )
   }
+
+  removeCharacter = index => {
+    const{characters} = this.state
+
+    this.setState({
+      characters: characters.filter((character,i) =>{
+        return i != index
+      }),
+    })
+  }
+
 }
 
 export default App
